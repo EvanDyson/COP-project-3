baff2f06dc0facaa05576983c010dbc12e5dbee2
EvanDyson <evandyson@ufl.edu>
2022-11-30 13:03:21 -0500

    updating files

--- Evan/ImGui 1.0/City Ranker/main.cpp
+++ Evan/ImGui 1.0/City Ranker/main.cpp
@@ -69,7 +69,7 @@ int main()
     //mergeSort(cVector, 0, 5);
     mergeSort(mergeArr, 0, 9);
  
-    radixSort(radixArr, 9);
+    radixSort(radixArr, 10);
     
     /*
     still need to test sorting the city vector based on scores
@@ -79,27 +79,56 @@ int main()
     after both are successful integrate into GUI to get user inputs for wanted outputs to use for specific weighted scores.
     test setting specific parameters and see if expected outputs are seen
     */
-
-
-// test printing the first 5 in the vector
-    for (int i = 0; i < 10; i++)
+    int option;
+    cout << "Pick 1 for Merge sort. Pick 2 for Radix sort.\n";
+    cin >> option;
+    if (option == 1)
     {
-        //cout << "Name: " << cVector[i]->name << endl;
-        //cout << "Population: " << cVector[i]->population << endl;
-        //cout << "Density: " << cVector[i]->density << endl;
-        //cout << "Size: " << cVector[i]->size << endl;
-        //cout << "Avg. Income: " << cVector[i]->avgIncome << endl;
-        //cout << "Avg. Housing Cost: " << cVector[i]->avgHousingCost << endl;
-        //// current weighted score is pop / 2
-        //cout << "Score: " << cVector[i]->weightScore << endl << endl;
+        // test printing the first 5 in the vector
+        cout << "\t\t==============  MERGE SORT  ===============\n\n";
+        for (int i = 0; i < 10; i++)
+        {
+            //cout << "Name: " << cVector[i]->name << endl;
+            //cout << "Population: " << cVector[i]->population << endl;
+            //cout << "Density: " << cVector[i]->density << endl;
+            //cout << "Size: " << cVector[i]->size << endl;
+            //cout << "Avg. Income: " << cVector[i]->avgIncome << endl;
+            //cout << "Avg. Housing Cost: " << cVector[i]->avgHousingCost << endl;
+            //// current weighted score is pop / 2
+            //cout << "Score: " << cVector[i]->weightScore << endl << endl;
 
-        cout << "CITY #" << i+1 << "'s name: " << mergeArr[i]->name << endl;
-        cout << "Population: " << mergeArr[i]->population << endl;
-        cout << "Density: " << mergeArr[i]->density << endl;
-        cout << "Size: " << mergeArr[i]->size << endl;
-        cout << "Avg. Income: " << mergeArr[i]->avgIncome << endl;
-        cout << "Avg. Housing Cost: " << mergeArr[i]->avgHousingCost << endl;
-        // current weighted score is pop / 2
-        cout << "Score: " << mergeArr[i]->weightScore << endl << endl;
+            cout << "CITY #" << i + 1 << "'s name: " << mergeArr[i]->name << endl;
+            cout << "Population: " << mergeArr[i]->population << endl;
+            cout << "Density: " << mergeArr[i]->density << endl;
+            cout << "Size: " << mergeArr[i]->size << endl;
+            cout << "Avg. Income: " << mergeArr[i]->avgIncome << endl;
+            cout << "Avg. Housing Cost: " << mergeArr[i]->avgHousingCost << endl;
+            // current weighted score is pop / 2
+            cout << "Score: " << mergeArr[i]->weightScore << endl << endl;
+        }
+    }
+    if (option == 2)
+    {
+        cout << "\t\t==============  RADIX SORT  ===============\n\n";
+        for (int i = 0; i < 10; i++)
+        {
+            //cout << "Name: " << cVector[i]->name << endl;
+            //cout << "Population: " << cVector[i]->population << endl;
+            //cout << "Density: " << cVector[i]->density << endl;
+            //cout << "Size: " << cVector[i]->size << endl;
+            //cout << "Avg. Income: " << cVector[i]->avgIncome << endl;
+            //cout << "Avg. Housing Cost: " << cVector[i]->avgHousingCost << endl;
+            //// current weighted score is pop / 2
+            //cout << "Score: " << cVector[i]->weightScore << endl << endl;
+
+            cout << "CITY #" << i + 1 << "'s name: " << radixArr[i]->name << endl;
+            cout << "Population: " << radixArr[i]->population << endl;
+            cout << "Density: " << radixArr[i]->density << endl;
+            cout << "Size: " << radixArr[i]->size << endl;
+            cout << "Avg. Income: " << radixArr[i]->avgIncome << endl;
+            cout << "Avg. Housing Cost: " << radixArr[i]->avgHousingCost << endl;
+            // current weighted score is pop / 2
+            cout << "Score: " << radixArr[i]->weightScore << endl << endl;
+        }
     }
 }
